7b702f09d190956af9db170949b6e18826c1f266
Dan Shai <danshai2013@gmail.com>
2018-09-14 07:04:20 +0300

    betaw-alphar

--- src/components/header.js
+++ src/components/header.js
@@ -4,16 +4,23 @@ import Menu from './menu'
 
 import mstyles from '../layouts/msite.module.css'
 const Header = ({ siteTitle }) => (
-  <div className={mstyles.coloredbar}  >
-    <div className={mstyles.mcontainer} >
+  <div className={mstyles.coloredbar}>
+    <div className={mstyles.namegt}>
       <h1>
-        <Link to="/" className={mstyles.mlinks} >
+        <Link to="/" className={mstyles.mlinks}>
           {siteTitle}
         </Link>
       </h1>
+      <p>
+        <a
+          href="https://github.com/DanShai/Gatsby-Machine-Learning-Starter"
+          className={mstyles.mlinks}
+        >
+          fork me on Github
+        </a>
+      </p>
     </div>
     <Menu />
-
   </div>
 )
 
