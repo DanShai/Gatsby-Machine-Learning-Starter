86aaa7bcb05bdf9bfcf6ab465e90127f554f757b
Dan Shai <danshai2013@gmail.com>
2018-09-14 07:41:43 +0300

    cs-icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Menu from './menu'
-
+import { FaGithub } from 'react-icons/fa'
 import mstyles from '../layouts/msite.module.css'
 const Header = ({ siteTitle }) => (
   <div className={mstyles.coloredbar}>
@@ -16,7 +16,10 @@ const Header = ({ siteTitle }) => (
           href="https://github.com/DanShai/Gatsby-Machine-Learning-Starter"
           className={mstyles.mlinks}
         >
-          fork me on Github
+          <h3>
+            {' '}
+            fork me on Github <FaGithub style={{ color: 'white' }} />
+          </h3>
         </a>
       </p>
     </div>
