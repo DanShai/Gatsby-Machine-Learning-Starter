a56817dee7cb4e88b3edc0155f0b0dc4978fa25a
Dan Shai <danshai2013@gmail.com>
2018-09-15 03:25:00 +0300

    cs-iconsss2

--- src/components/header.js
+++ src/components/header.js
@@ -6,25 +6,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import mstyles from '../layouts/msite.module.css'
 
-const Header = ({ siteTitle }) => (
-  <div className={mstyles.coloredbar}>
-    <div className={mstyles.namegt}>
-      <h1>
-        <Link to="/" className={mstyles.mlinks}>
-          {siteTitle}
-        </Link>
-      </h1>
-      <p className={mstyles.wlink}>
-        <a
-          href="https://github.com/DanShai/Gatsby-Machine-Learning-Starter"
-          className={mstyles.mlinks}
-        >
-          fork me on Github <FontAwesomeIcon icon={faGithub} color="white" />
-        </a>
-      </p>
-    </div>
-    <Menu />
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { loaded: false }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loaded: true })
+    }, 1500)
+  }
+  render() {
+    return (
+      <div className={mstyles.coloredbar}>
+        <div className={mstyles.namegt}>
+          <h1>
+            <Link to="/" className={mstyles.mlinks}>
+              {this.props.siteTitle}
+            </Link>
+          </h1>
+          {this.state.loaded && (
+            <span className={mstyles.wlink}>
+              <a
+                href="https://github.com/DanShai/Gatsby-Machine-Learning-Starter"
+                className={mstyles.mlinks}
+              >
+                fork me on Github{' '}
+                <FontAwesomeIcon icon={faGithub} color="white" />
+              </a>
+            </span>
+          )}
+        </div>
+        <Menu />
+      </div>
+    )
+  }
+}
 
 export default Header
